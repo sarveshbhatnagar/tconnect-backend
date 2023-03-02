5cb138635dc0f774352dbec5c8c31dc0290f2de6
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2023-03-02 18:38:06 -0500

    updating the personal details tests

--- test/backend/info/personalDetails.test.ts
+++ test/backend/info/personalDetails.test.ts
@@ -15,3 +15,38 @@ describe('PersonalDetails', () => {
         expect (personalDetails.username).toBe('testUsername');
     });
 });
+
+
+describe('PersonalDetails: from', () => {
+    it('should build a PersonalDetails object from an object', () => {
+        const userObject = {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'jdjs@msm.com',
+            phone: '1234567890',
+            username: 'testUsername',
+            somethingelse: 'something'
+        }
+        const personalDetails = PersonalDetailsBuilder.from(userObject);
+        expect (personalDetails.firstName).toBe('John');
+        expect (personalDetails.lastName).toBe('Doe');
+        expect (personalDetails.email).toBe('jdjs@msm.com');
+        expect (personalDetails.phone).toBe('1234567890');
+        expect (personalDetails.username).toBe('testUsername');
+    });
+
+    it('should build a PersonalDetails object from an object with missing fields', () => {
+        const userObject = {
+            firstName: 'John',
+            lastName: 'Doe',
+            username: 'testUsername',
+            somethingelse: 'something'
+        }
+        const personalDetails = PersonalDetailsBuilder.from(userObject);
+        expect (personalDetails.firstName).toBe('John');
+        expect (personalDetails.lastName).toBe('Doe');
+        expect (personalDetails.email).toBeUndefined();
+        expect (personalDetails.phone).toBeUndefined();
+        expect (personalDetails.username).toBe('testUsername');
+    });
+});
