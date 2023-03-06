ab9259308c1e4bc88fed30a938822efc6e3a3d71
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2023-03-06 17:58:54 -0500

    adding scripts

--- lib/stacks/tconnect-backend-stack.ts
+++ lib/stacks/tconnect-backend-stack.ts
@@ -9,7 +9,7 @@ export class LambdaStack extends Stack {
     const lambdaFunction = new Function(this, 'BackendLambdaFunction', {
       runtime: Runtime.NODEJS_14_X,
       handler: 'index.handler',
-      code: Code.fromAsset('./dist/backend/'),
+      code: Code.fromAsset('./dist/'),
     });
   }
 }
