b46dbc36f5a3fd887e479b7bedcd09542e808454
Pratyashshrivastava <kush.shrivastava12@gmail.com>
2023-04-01 23:52:26 +0530

    Added provider and databaseSchema

---
+++ lib/backend/repository/tables/databaseSchema.ts
@@ -0,0 +1,29 @@
+abstract class DatabaseSchema {
+    private constructor() {
+        throw new Error("Database is an abstract class");
+    }
+
+    static getInstance() {
+        throw new Error("Method 'getInstance()' must be implemented.");
+    }
+
+    static get(){
+        throw new Error("Method 'getData()' must be implemented.");
+    }
+
+    static put(){
+        throw new Error("Method 'putData()' must be implemented.");
+    }
+
+    static update(){
+        throw new Error("Method 'updateData()' must be implemented.");
+    }
+
+    static delete(){
+        throw new Error("Method 'deleteData()' must be implemented.");
+    }
+
+
+}
+
+export { DatabaseSchema };
